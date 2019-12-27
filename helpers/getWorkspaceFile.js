95af324ed5a0bf183c57efe4fae1d9927e2da344
Alex Barashkov <lnikell@gmail.com>
2019-12-27 11:56:40 +0100

    Add drone ci configuration

--- helpers/getWorkspaceFile.js
+++ helpers/getWorkspaceFile.js
@@ -1,5 +1,6 @@
-const vscode = require('vscode')
+// eslint-disable-next-line import/no-unresolved
+const vscode = require('vscode');
 
 // takes a filename and returns Promise obj
 // (fileName: String) -> Promise
-module.exports = (fileName = '') => vscode.workspace.findFiles(fileName, 1)
+module.exports = (fileName = '') => vscode.workspace.findFiles(fileName, 1);
